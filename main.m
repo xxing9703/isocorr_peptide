107883b7dccaa75854dd7808cb2fb493422c4460
Xi Xing <xxing@princeton.edu>
2021-08-04 16:26:08 -0400

    fixed issues 1-2

--- main.m
+++ main.m
@@ -1,5 +1,7 @@
 % this script is for single peptide test run, with plot 
-fname='m27c_Liver.csv';
+clear
+fname='m27c_Liver_test.csv';
+[~,fn]=fileparts(fname);
 T=readtable(fname);
 
 %%
@@ -7,20 +9,24 @@ for i=1:size(T,1);  % loop over peptides
     i
 pep=T{i,13}; %peptide sequence location column 13
 pep=pep{1};
-
-MID_measure=T{i,16:26};
+mask = startsWith(T.Properties.VariableNames, 'Area');
+MID_measure=T{i,mask};
+poolsize=sum(MID_measure);
 MID_measure=MID_measure/sum(MID_measure); %normalize to 0-1
 [MID_sim,MID_corr,fval]=pepcorr(pep,MID_measure);
 
 MID_simcorr=conv(MID_corr,MID_sim);
 MID_simcorr=MID_simcorr(1:length(MID_measure));
-out(i).corr=MID_corr;
+out(i).sum=poolsize;
+out(i).corrM_0=MID_corr(1);
+out(i).corrM=MID_corr(2:end);
 out(i).err=fval;
 out2(i).sim=MID_sim;
 out2(i).simcorr=MID_simcorr;
 end
-
-writetable(struct2table(out),'output.csv')
+Tout=struct2table(out);
+Tout=[T(:,~mask),Tout];
+writetable(Tout,[fn,'_corrected.csv']);
 
 %%
 % figure
